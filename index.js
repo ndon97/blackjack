fc63f8416fd2e8c0a42cf271f1a4e28e26caeb94
Nick Don <nick.don.1102@gmail.com>
2022-08-27 12:17:19 +1000

    create basic working game with console

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 let firstCard = 6
 let secondCard = 5
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-if (sum < 21) {
-  console.log("Do you want to draw a new card ")
-} else if (sum === 21) {
-  console.log ("Wohoo!, You've got Blackjack!")
-} else if (sum > 21) {
-  console.log("You're out of the game")
+function startGame() {
+    if (sum <= 20) {
+      message = "Do you want to draw a new card "
+    } else if (sum === 21) {
+      message = "Wohoo!, You've got Blackjack!"
+      hasBlackJack = true
+    } else {
+      message = "You're out of the game"
+      isAlive = false
+    }
 }
